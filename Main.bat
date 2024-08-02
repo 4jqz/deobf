c2d6143836f48bd1bbc786b9c8f647f8745d16c6
Larp <135446603+LarpsWorld@users.noreply.github.com>
2024-08-02 17:51:44 +0100

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -1,28 +1,31 @@
 @echo off
 echo Installing required Python packages...
 
-pip install requests
-pip install mega.py
+pip install requests mega.py ttkbootstrap Pillow
+cls
+pip show requests
+timeout /t 3 /nobreak > nul
+cls
+pip show mega.py
+timeout /t 3 /nobreak > nul
+cls
+pip show ttkbootstrap 
+timeout /t 3 /nobreak > nul
+cls
+pip show pillow
+timeout /t 3 /nobreak >nul
+cls
 
-REM Check if tkinter is installed (for Windows, assuming Python is installed in the default location)
+
+REM Check if tkinter is installed
 python -c "import tkinter" 2>NUL
 IF ERRORLEVEL 1 (
     echo Installing tkinter...
-    REM Install tkinter using the appropriate method for your system
     python -m pip install tk
 ) ELSE (
     echo tkinter is already installed.
 )
 
-REM Check if ctypes is installed (part of standard library, so this is usually not necessary)
-python -c "import ctypes" 2>NUL
-IF ERRORLEVEL 1 (
-    echo ctypes is part of the standard library and should be installed with Python.
-    REM If it's not installed, provide instructions or install it
-) ELSE (
-    echo ctypes is already installed.
-)
-
 echo Running the Deobfuscator GUI...
-python path\to\your\script.py
+python "%~dp0gui2.py"
 pause
